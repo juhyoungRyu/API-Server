8da861b6da3cc6d88410486801cbcbdcd7a845a1
jh.ryu <jh.ryu@quintet.co.kr>
2024-08-05 15:51:23 +0900

    :sparkles: Template

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express, { Request, Response, Router } from 'express'
 import { getUuid } from './src/utils/projectUtil'
 
 import { ObjReturn } from './src/interface'
-import { createCipheriv } from 'crypto'
 
 const app = express()
 app.use(express.json()) // JSON 본문 파싱 미들웨어 추가
@@ -13,24 +12,12 @@ app.listen(8080, () => {
 
 const dataApi: Router = express.Router()
 
-dataApi.post('/create', (req: Request, res: Response) => {
-  const secret = req.body.uuid
-  const iv = (() => {
-    const base = req.headers.authorization as string
-    const signature = base.split('.')[2]
-
-    return signature.slice(-16)
-  })()
-
-  const cipher = createCipheriv('aes-256-cbc', secret, iv)
-  const encData = cipher.update(req.body.saveData, 'utf8', 'hex').concat(cipher.final('hex'))
+dataApi.post('/', (req: Request, res: Response) => {
 
   const objReturn: ObjReturn = {
     success: true,
     data: {
-      secret,
-      iv,
-      encData
+      id: getUuid()
     },
     error: {
       code: 0,
