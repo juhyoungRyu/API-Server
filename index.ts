c4590b22c2d6ab3c4e0cedafddc69907e63f1ad5
jh.ryu <jh.ryu@quintet.co.kr>
2024-08-05 15:33:04 +0900

    :sparkles: CbcEncrypt Test

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from 'express'
 import { getUuid } from './src/utils/projectUtil'
 
 import { ObjReturn } from './src/interface'
+import { createCipheriv } from 'crypto'
 
 const app = express()
 app.use(express.json()) // JSON 본문 파싱 미들웨어 추가
@@ -13,13 +14,23 @@ app.listen(8080, () => {
 const dataApi: Router = express.Router()
 
 dataApi.post('/create', (req: Request, res: Response) => {
-  const name: string = req.body.name
+  const secret = req.body.uuid
+  const iv = (() => {
+    const base = req.headers.Authorization as string
+    const signature = base.split('/')[2]
+
+    return signature.slice(-16)
+  })()
+
+  const cipher = createCipheriv('aes-256-cbc', secret, iv)
+  const encData = cipher.update(req.body.saveData, 'utf8', 'hex').concat(cipher.final('hex'))
 
   const objReturn: ObjReturn = {
     success: true,
     data: {
-      id: getUuid(),
-      name: `${name.replace('S3', 'S4')} (Rest API)`
+      secret,
+      iv,
+      encData
     },
     error: {
       code: 0,
